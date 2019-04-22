bf8f997047433bacf324ba8ebf504791a19be698
ResidentMario <aleksey.bilogur@gmail.com>
2019-04-22 15:48:53 -0700

    Set name in setup.py.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='gtfs-tripify',
+    name='gtfs_tripify',
     py_modules=['gtfs_tripify'],
     version='0.0.1',
     install_requires=['numpy', 'pandas'],
