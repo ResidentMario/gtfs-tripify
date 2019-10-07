585aa48a97afdc31fa3c38a2bf19f5a14a73165b
Aleksey Bilogur <aleksey.bilogur@gmail.com>
2019-10-06 20:03:29 -0400

    Implement a CLI. (#10)

--- setup.py
+++ setup.py
@@ -5,15 +5,19 @@ setup(
     packages=['gtfs_tripify'],    
     py_modules=['gtfs_tripify'],
     version='0.0.3',
-    install_requires=['numpy', 'pandas', 'requests', 'gtfs-realtime-bindings'],
+    install_requires=['numpy', 'pandas', 'requests', 'gtfs-realtime-bindings', 'click'],
     extras_require={
         'develop': ['pytest']
     },
-    description='TODO.',
+    entry_points='''
+        [console_scripts]
+        gtfs_tripify=gtfs_tripify.cli:cli
+    ''',
+    description='Turn GTFS-RT transit updates into historical arrival data.',
     author='Aleksey Bilogur',
     author_email='aleksey.bilogur@gmail.com',
     url='https://github.com/ResidentMario/gtfs-tripify',
-    download_url='https://github.com/ResidentMario/gtfs-tripify/tarball/0.0.1',
+    download_url='https://github.com/ResidentMario/gtfs-tripify/tarball/0.0.3',
     keywords=['TODO'],
     classifiers=[]
 )
